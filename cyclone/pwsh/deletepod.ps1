a6c7d0c3b2df70a88ede17003d11538ed17e2d32
evanjd711 <evanjd711@gmail.com>
2023-11-26 20:21:58 -0800

    goodbye

--- cyclone/pwsh/deletepod.ps1
+++
@@ -1,8 +0,0 @@
-param(
-[String] $Username,
-[String] $Tag
-)
-
-$cred = Import-CliXML -Path ./lib/creds/vsphere_cred.xml
-Connect-VIServer elsa.sdc.cpp -Credential $cred
-Invoke-OrderSixtySix -Username $Username -Tag $Tag
